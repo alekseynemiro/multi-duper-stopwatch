353aa680662689bbf7655eede440f32882f29e40
Aleksey Nemiro <aleksey@kbyte.ru>
2023-07-06 18:14:11 +0300

    Colors - Extends and improvements.

--- src/Styles/Colors.ts
+++ src/Styles/Colors.ts
@@ -1,16 +1,90 @@
+const white = "#ffffff";
+const black = "#000000";
+const primary = "#0d6efd";
+const secondary = "#6c757d";
+const danger = "#dc3545";
+const warning = "#ffc107";
+const success = "#198754";
+const info = "#0dcaf0";
+const border = black;
+
 export const colors = {
-  default: "",
-  primary: "#0d6efd",
-  secondary: "#6c757d",
-  danger: "#dc3545",
-  warning: "#ffc107",
-  success: "#198754",
-  info: "#0dcaf0",
-  defaultContrast: "#000000",
-  primaryContrast: "#ffffff",
-  secondaryContrast: "#ffffff",
-  dangerContrast: "#ffffff",
-  warningContrast: "#000000",
-  successContrast: "#ffffff",
-  infoContrast: "#000000",
+  white,
+  black,
+
+  primary,
+  secondary,
+  danger,
+  warning,
+  success,
+  info,
+
+  border,
+
+  primaryContrast: white,
+  secondaryContrast: white,
+  dangerContrast: white,
+  warningContrast: black,
+  successContrast: white,
+  infoContrast: black,
+
+  shadowColor: black,
+
+  palette: [
+    "#faa0a0",
+    "#f08080",
+    "#ff0000",
+    "#e3242b",
+    "#990f02",
+    "#60100b",
+
+    "#ffdab9",
+    "#f4a460",
+    "#fc851e",
+    "#d2691e",
+    "#8b4513",
+    "#542a0c",
+
+    "#ffebcd",
+    "#e6ce81",
+    "#ffd700",
+    "#daa520",
+    "#b8860b",
+    "#7d5b09",
+
+    "#98fb98",
+    "#00ff00",
+    "#32cd32",
+    "#008000",
+    "#006400",
+    "#013d01",
+
+    "#92f9fc",
+    "#05f5fc",
+    "#02d5db",
+    "#02b7bd",
+    "#018d91",
+    "#01484a",
+
+    "#a6b8ff",
+    "#6181ff",
+    "#0033ff",
+    "#002ad1",
+    "#022099",
+    "#000e47",
+
+    "#f9b8ff",
+    "#ee6bfa",
+    "#ea00ff",
+    "#b600c7",
+    "#6c0275",
+    "#38003d",
+
+    "#ffffff",
+    "#e6e6e6",
+    "#cccccc",
+    "#777777",
+    "#333333",
+    "#000000",
+  ],
 };
