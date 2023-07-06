19cd5c540118d0d280840c2864c4caef005accbd
Aleksey Nemiro <aleksey@kbyte.ru>
2023-07-06 20:37:27 +0300

    Icon - Fixed "Cannot read property 'charAt' of undefined".

--- src/Components/Icon/Icon.tsx
+++ src/Components/Icon/Icon.tsx
@@ -9,8 +9,8 @@ export function Icon({ name, variant, size }: IconProps): JSX.Element {
 
   if (variant) {
     const part1 = variant.split("-")[0];
-    const part2 = variant.split("-")[1].charAt(0).toUpperCase() + variant.split("-")[1].slice(1);
-    color = (colors as any)[part1 + (part2 ?? "")] ?? colors.text;
+    const part2 = (variant.split("-")[1]?.charAt(0).toUpperCase() ?? "") + (variant.split("-")[1]?.slice(1) ?? "");
+    color = (colors as any)[part1 + part2] ?? colors.text;
   }
 
   return (
