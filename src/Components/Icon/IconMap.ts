cb5dd689b634ab51f79ac0cbede63520ff460c71
Aleksey Nemiro <aleksey@kbyte.ru>
2023-07-24 16:14:51 +0300

    IconName - Added "exit".

--- src/Components/Icon/IconMap.ts
+++ src/Components/Icon/IconMap.ts
@@ -2,4 +2,5 @@ import { IconName } from "./IconName";
 
 export const iconMap = new Map<IconName, string>()
   .set("delete", "trash")
-  .set("color", "paint-brush");
+  .set("color", "paint-brush")
+  .set("exit", "sign-out-alt");
--- src/Components/Icon/IconName.ts
+++ src/Components/Icon/IconName.ts
@@ -4,4 +4,5 @@ export type IconName =
   | "color"
   | "grip-lines"
   | "play"
-  | "pause";
+  | "pause"
+  | "exit";
