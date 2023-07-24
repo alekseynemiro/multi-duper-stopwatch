bdc0f893be210fa437f64e1d25cdb181237732e2
Aleksey Nemiro <aleksey@kbyte.ru>
2023-07-24 13:15:43 +0300

    Button - Merged imports.

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
-import { View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { ButtonProps } from "./ButtonProps";
 import { buttonStyles } from "./ButtonStyles";
 
