8221260b081e437f8b3beb7ea43f78370c911405
Aleksey Nemiro <aleksey@kbyte.ru>
2023-07-26 19:56:45 +0300

    ISessionService.pause - deprecated.

--- src/Core/Services/Sessions/ISessionService.ts
+++ src/Core/Services/Sessions/ISessionService.ts
@@ -13,6 +13,10 @@ export interface ISessionService {
 
   toggle(request: ToggleRequest): Promise<ToggleResult>;
 
+  /**
+   * @deprecated It looks like this method is no longer needed.
+   * Please delete if enough time has passed - 2023-07-26.
+   */
   pause(request: PauseRequest): Promise<void>;
 
   finish(request: FinishRequest): Promise<void>;
