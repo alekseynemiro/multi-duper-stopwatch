149642e0f3d89de2980c41fb2e5b069e28aae1ca
Aleksey Nemiro <aleksey@kbyte.ru>
2023-07-26 18:14:14 +0300

    ElapsedTime - Using getTimeSpan for default state.

--- src/Views/ActiveProject/Components/StopwatchDisplay/ElapsedTime.tsx
+++ src/Views/ActiveProject/Components/StopwatchDisplay/ElapsedTime.tsx
@@ -3,19 +3,13 @@ import { Text } from "react-native";
 import { ServiceIdentifier, serviceProvider } from "@config";
 import { IStopwatchService, StopwatchTickEventArgs } from "@services/Stopwatch";
 import { TimeSpan } from "@types";
+import { getTimeSpan } from "@utils/TimeUtils";
 import { stopwatchDisplayStyles } from "./StopwatchDisplayStyles";
 
 const stopwatchService = serviceProvider.get<IStopwatchService>(ServiceIdentifier.StopwatchService);
 
 export function ElapsedTime(): JSX.Element {
-  const [elapsed, setElapsed] = useState<TimeSpan>({
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-    milliseconds: 0,
-    displayValue: "",
-  });
+  const [elapsed, setElapsed] = useState<TimeSpan>(getTimeSpan(0));
 
   useEffect(
     (): { (): void } => {
