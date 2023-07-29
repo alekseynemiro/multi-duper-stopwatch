d96935da88779e0acfb2434c89438df9122c16c1
Aleksey Nemiro <aleksey@kbyte.ru>
2023-07-29 23:36:50 +0300

    HomePage - Add open and create project buttons

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,11 +1,16 @@
-import React, { useRef, useState } from "react";
-import { useWindowDimensions } from "react-native";
+import React, { useCallback, useRef, useState } from "react";
+import { Text, useWindowDimensions,View } from "react-native";
 import Carousel from "react-native-reanimated-carousel";
-import { Routes } from "@config";
+import { Button } from "@components/Button";
+import { Routes, ServiceIdentifier, serviceProvider } from "@config";
+import { useFocusEffect } from "@react-navigation/native";
+import { IProjectService } from "@services/Projects";
 import { useNavigation, useRoute } from "@utils/NavigationUtils";
 import { ActiveProjectView } from "@views/ActiveProject";
 import { ReportView, ReportViewProps } from "@views/Report";
 import { homePageStyles } from "./HomePageStyles";
+
+const projectService = serviceProvider.get<IProjectService>(ServiceIdentifier.ProjectService);
 
 export function HomePage(): JSX.Element {
   const { width } = useWindowDimensions();
@@ -17,6 +22,58 @@ export function HomePage(): JSX.Element {
   const reportViewRef = useRef<ReportViewProps>();
 
   const [sessionId, setSessionId] = useState<string | undefined>();
+  const [canOpenProject, setCanOpenProject] = useState<boolean>(false);
+
+  const load = useCallback(
+    async(): Promise<void> => {
+      const projects = await projectService.getAll();
+
+      setCanOpenProject(projects.items.length > 0);
+    },
+    []
+  );
+
+  useFocusEffect(
+    useCallback(
+      (): void => {
+        load();
+      },
+      [
+        load,
+      ]
+    )
+  );
+
+  if (!projectId) {
+    return (
+      <View
+        style={homePageStyles.openProject}
+      >
+        <Button
+          variant="secondary"
+          title="Create new project"
+          style={homePageStyles.createProjectButton}
+          onPress={(): void => {
+            navigation.navigate(Routes.Project);
+          }}
+        />
+        <Text
+          style={homePageStyles.text}
+        >
+          - or -
+        </Text>
+        <Button
+          variant="secondary"
+          title="Open project"
+          disabled={!canOpenProject}
+          style={homePageStyles.openProjectButton}
+          onPress={(): void => {
+            navigation.navigate(Routes.ProjectList);
+          }}
+        />
+      </View>
+    );
+  }
 
   const activeProjectView = (
     <ActiveProjectView
--- src/Pages/Home/HomePageStyles.ts
+++ src/Pages/Home/HomePageStyles.ts
@@ -4,4 +4,19 @@ export const homePageStyles = StyleSheet.create({
   container: {
     height: "100%",
   },
+  openProject: {
+    marginTop: "50%",
+    justifyContent: "center",
+    alignContent: "center",
+    marginHorizontal: "10%",
+  },
+  openProjectButton: {
+    marginVertical: 8,
+  },
+  createProjectButton: {
+    marginVertical: 8,
+  },
+  text: {
+    textAlign: "center",
+  },
 });
