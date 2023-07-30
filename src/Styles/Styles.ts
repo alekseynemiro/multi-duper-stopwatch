e3ed1ab21de1817665fd601f17ef06abf4a1796b
Aleksey Nemiro <aleksey@kbyte.ru>
2023-07-30 18:25:20 +0300

    Styles - Cleanup

--- src/Styles/Styles.ts
+++ src/Styles/Styles.ts
@@ -2,22 +2,6 @@ import { StyleSheet } from "react-native";
 import { colors } from "./Colors";
 
 export const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-  },
-  highlight: {
-    fontWeight: "700",
-  },
   table: {
     flex: 1,
     alignItems: "center",
