758bc21f5aff5eada810c65575dd6cb2df3d6336
Aleksey Nemiro <aleksey@kbyte.ru>
2023-08-05 23:27:10 +0300

    AboutPage - Use getApplicationName
    Changing the Android resources
    solved the problem with the correct output
    of the application name.

--- src/Pages/About/AboutPage.tsx
+++ src/Pages/About/AboutPage.tsx
@@ -22,7 +22,7 @@ export function AboutPage(): JSX.Element {
           source={AppIcon}
         />
         <Text>
-          Multi-duper Stopwatch
+          {DeviceInfo.getApplicationName()}
         </Text>
         <Text>
           v{DeviceInfo.getVersion() ?? "0"}
