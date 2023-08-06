04892cc978058632bc46f799ff3a9a2794c9627a
Aleksey Nemiro <aleksey@kbyte.ru>
2023-08-06 19:28:11 +0300

    Colors - Improve using color contrast checker

--- src/Styles/Colors.ts
+++ src/Styles/Colors.ts
@@ -1,10 +1,10 @@
 const white = "#ffffff";
 const black = "#000000";
-const primary = "#0d6efd";
-const secondary = "#6c757d";
-const danger = "#dc3545";
+const primary = "#1458b1";
+const secondary = "#555555";
+const danger = "#a92532";
 const warning = "#ffc107";
-const success = "#198754";
+const success = "#14653f";
 const info = "#0dcaf0";
 const light = "#f8f9fa";
 const transparent = "transparent";
@@ -50,59 +50,59 @@ export const colors = {
   shadowColor: black,
 
   palette: [
-    { color: "#faa0a0", contrast: "#60100b" },
-    { color: "#f08080", contrast: "#60100b" },
-    { color: "#ff0000", contrast: "#fcd7d7" },
-    { color: "#e3242b", contrast: "#fcd7d7" },
-    { color: "#990f02", contrast: "#fcd7d7" },
-    { color: "#60100b", contrast: "#fcd7d7" },
+    { color: "#faaaaa", contrast: "#60100b" },
+    { color: "#f08080", contrast: "#2a0806" },
+    { color: "#f55c5c", contrast: "#000000" },
+    { color: "#e3242b", contrast: "#ffffff" },
+    { color: "#990f02", contrast: "#fff8f8" },
+    { color: "#670900", contrast: "#fff8f8" },
 
     { color: "#ffdab9", contrast: "#542a0c" },
-    { color: "#f4a460", contrast: "#542a0c" },
-    { color: "#fc851e", contrast: "#542a0c" },
-    { color: "#d2691e", contrast: "#ffdab9" },
-    { color: "#8b4513", contrast: "#ffdab9" },
-    { color: "#542a0c", contrast: "#ffdab9" },
+    { color: "#f4a460", contrast: "#3d1d06" },
+    { color: "#fc851e", contrast: "#1d0c01" },
+    { color: "#d2691e", contrast: "#000000" },
+    { color: "#8b4513", contrast: "#ffffff" },
+    { color: "#542a0c", contrast: "#f8e4d2" },
 
-    { color: "#ffebcd", contrast: "#7d5b09" },
-    { color: "#e6ce81", contrast: "#7d5b09" },
-    { color: "#ffd700", contrast: "#7d5b09" },
-    { color: "#daa520", contrast: "#fae5b1" },
-    { color: "#b8860b", contrast: "#ffebcd" },
-    { color: "#7d5b09", contrast: "#ffebcd" },
+    { color: "#ffebcd", contrast: "#604607" },
+    { color: "#e6ce81", contrast: "#443106" },
+    { color: "#ffd700", contrast: "#473407" },
+    { color: "#daa520", contrast: "#1d1500" },
+    { color: "#b8860b", contrast: "#000000" },
+    { color: "#7d5b09", contrast: "#ffffff" },
 
     { color: "#98fb98", contrast: "#013d01" },
     { color: "#00ff00", contrast: "#013d01" },
-    { color: "#32cd32", contrast: "#013d01" },
-    { color: "#008000", contrast: "#98fb98" },
-    { color: "#006400", contrast: "#deffde" },
-    { color: "#013d01", contrast: "#deffde" },
+    { color: "#32cd32", contrast: "#002a00" },
+    { color: "#008000", contrast: "#ffffff" },
+    { color: "#006400", contrast: "#effcef" },
+    { color: "#013d01", contrast: "#effcef" },
 
     { color: "#92f9fc", contrast: "#01484a" },
     { color: "#05f5fc", contrast: "#01484a" },
-    { color: "#02d5db", contrast: "#01484a" },
-    { color: "#02b7bd", contrast: "#bef8fa" },
-    { color: "#018d91", contrast: "#92f9fc" },
-    { color: "#01484a", contrast: "#92f9fc" },
+    { color: "#02d5db", contrast: "#003638" },
+    { color: "#02b7bd", contrast: "#001c1d" },
+    { color: "#018d91", contrast: "#000000" },
+    { color: "#006C6F", contrast: "#ffffff" },
 
     { color: "#a6b8ff", contrast: "#000e47" },
-    { color: "#6181ff", contrast: "#000e47" },
-    { color: "#0033ff", contrast: "#a6b8ff" },
-    { color: "#002ad1", contrast: "#a6b8ff" },
-    { color: "#022099", contrast: "#a6b8ff" },
-    { color: "#000e47", contrast: "#a6b8ff" },
+    { color: "#6181ff", contrast: "#000000" },
+    { color: "#0033ff", contrast: "#fcfcff" },
+    { color: "#002ad1", contrast: "#ededfa" },
+    { color: "#022099", contrast: "#c6d1ff" },
+    { color: "#000e47", contrast: "#bfcbfc" },
 
     { color: "#f9b8ff", contrast: "#38003d" },
-    { color: "#ee6bfa", contrast: "#38003d" },
-    { color: "#ea00ff", contrast: "#38003d" },
-    { color: "#b600c7", contrast: "#f9b8ff" },
-    { color: "#6c0275", contrast: "#f9b8ff" },
-    { color: "#38003d", contrast: "#f9b8ff" },
+    { color: "#ee6bfa", contrast: "#250229" },
+    { color: "#ea00ff", contrast: "#020002" },
+    { color: "#b600c7", contrast: "#ffffff" },
+    { color: "#6c0275", contrast: "#f7d1fa" },
+    { color: "#38003d", contrast: "#f9c5fd" },
 
     { color: "#ffffff", contrast: "#333333" },
     { color: "#e6e6e6", contrast: "#333333" },
     { color: "#cccccc", contrast: "#333333" },
-    { color: "#777777", contrast: "#e6e6e6" },
+    { color: "#777777", contrast: "#000000" },
     { color: "#333333", contrast: "#ffffff" },
     { color: "#000000", contrast: "#ffffff" },
   ] as Array<{ color: string, contrast: string }>,
