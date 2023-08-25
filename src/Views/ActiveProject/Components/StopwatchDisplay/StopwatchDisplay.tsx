ef9d812ae1c3ab0dcb11066b68099127946d3413
Aleksey Nemiro <aleksey@kbyte.ru>
2023-08-25 20:24:41 +0300

    StopwatchDisplay - Fix typing issue

--- src/Views/ActiveProject/Components/StopwatchDisplay/StopwatchDisplay.tsx
+++ src/Views/ActiveProject/Components/StopwatchDisplay/StopwatchDisplay.tsx
@@ -6,6 +6,7 @@ import {
   useLocalizationService,
   useSessionStorageService,
 } from "@config";
+import { ColorPalette } from "@data";
 import { Activity as ActivityModel } from "@dto/ActiveProject";
 import { colors } from "@styles";
 import { SessionStorageKeys } from "@types";
@@ -97,10 +98,10 @@ export function StopwatchDisplay(props: StopwatchDisplayProps): JSX.Element {
                       stopwatchDisplayStyles.mode,
                       {
                         backgroundColor: currentActivity?.color
-                          ? getColorCode((currentActivity as ActivityModel).color)
+                          ? getColorCode((currentActivity as ActivityModel).color as ColorPalette)
                           : colors.white,
                         color: currentActivity?.color
-                          ? getContrastColorCode((currentActivity as ActivityModel).color)
+                          ? getContrastColorCode((currentActivity as ActivityModel).color as ColorPalette)
                           : colors.text,
                       },
                     ]}
