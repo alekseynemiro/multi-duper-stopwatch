9071510ddc5652e1c512edef6fa58ec959c460db
Aleksey Nemiro <aleksey@kbyte.ru>
2023-08-25 16:36:09 +0300

    ActivityPopupMenu - Close by press on backdrop

--- src/Views/ActiveProject/Components/ActivityPopupMenu/ActivityPopupMenu.tsx
+++ src/Views/ActiveProject/Components/ActivityPopupMenu/ActivityPopupMenu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Text, View } from "react-native";
+import { Modal, Text, TouchableWithoutFeedback, View } from "react-native";
 import { Button } from "@components/Button";
 import { Icon } from "@components/Icon";
 import { useLocalizationService } from "@config";
@@ -20,78 +20,86 @@ export function ActivityPopupMenu(props: ActivityPopupMenuProps): JSX.Element {
       transparent={true}
       visible={true}
     >
-      <View style={activityPopupMenuStyles.centeredView}>
-        <View style={activityPopupMenuStyles.modalView}>
-          <Button
-            variant="light"
-            childWrapperStyle={activityPopupMenuStyles.buttonChildContainer}
-            onPress={(): void => {
-              onPress({
-                action: "add",
-              });
-            }}
-          >
-            <Icon
-              name="add"
-              style={activityPopupMenuStyles.buttonIcon}
-            />
-            <Text>{localization.get("activeProject.activityPopupMenu.add")}</Text>
-          </Button>
-          <Button
-            variant="light"
-            childWrapperStyle={activityPopupMenuStyles.buttonChildContainer}
-            onPress={(): void => {
-              onPress({
-                action: "edit",
-                activityId: activityId as string,
-              });
-            }}
-          >
-            <Icon
-              name="edit"
-              style={activityPopupMenuStyles.buttonIcon}
-            />
-            <Text>{localization.get("activeProject.activityPopupMenu.edit")}</Text>
-          </Button>
-          <Button
-            variant="light"
-            childWrapperStyle={activityPopupMenuStyles.buttonChildContainer}
-            onPress={(): void => {
-              onPress({
-                action: "delete",
-                activityId: activityId as string,
-              });
-            }}
-            onLongPress={(): void => {
-              onPress({
-                action: "delete-forced",
-                activityId: activityId as string,
-              });
-            }}
-          >
-            <Icon
-              name="delete"
-              style={activityPopupMenuStyles.buttonIcon}
-            />
-            <Text>{localization.get("activeProject.activityPopupMenu.delete")}</Text>
-          </Button>
-          <Button
-            variant="light"
-            childWrapperStyle={activityPopupMenuStyles.buttonChildContainer}
-            onPress={(): void => {
-              onPress({
-                action: "cancel",
-              });
-            }}
-          >
-            <Icon
-              name="cancel"
-              style={activityPopupMenuStyles.buttonIcon}
-            />
-            <Text>{localization.get("activeProject.activityPopupMenu.cancel")}</Text>
-          </Button>
+      <TouchableWithoutFeedback
+        onPress={(): void => {
+          onPress({
+            action: "cancel",
+          });
+        }}
+      >
+        <View style={activityPopupMenuStyles.centeredView}>
+          <View style={activityPopupMenuStyles.modalView}>
+            <Button
+              variant="light"
+              childWrapperStyle={activityPopupMenuStyles.buttonChildContainer}
+              onPress={(): void => {
+                onPress({
+                  action: "add",
+                });
+              }}
+            >
+              <Icon
+                name="add"
+                style={activityPopupMenuStyles.buttonIcon}
+              />
+              <Text>{localization.get("activeProject.activityPopupMenu.add")}</Text>
+            </Button>
+            <Button
+              variant="light"
+              childWrapperStyle={activityPopupMenuStyles.buttonChildContainer}
+              onPress={(): void => {
+                onPress({
+                  action: "edit",
+                  activityId: activityId as string,
+                });
+              }}
+            >
+              <Icon
+                name="edit"
+                style={activityPopupMenuStyles.buttonIcon}
+              />
+              <Text>{localization.get("activeProject.activityPopupMenu.edit")}</Text>
+            </Button>
+            <Button
+              variant="light"
+              childWrapperStyle={activityPopupMenuStyles.buttonChildContainer}
+              onPress={(): void => {
+                onPress({
+                  action: "delete",
+                  activityId: activityId as string,
+                });
+              }}
+              onLongPress={(): void => {
+                onPress({
+                  action: "delete-forced",
+                  activityId: activityId as string,
+                });
+              }}
+            >
+              <Icon
+                name="delete"
+                style={activityPopupMenuStyles.buttonIcon}
+              />
+              <Text>{localization.get("activeProject.activityPopupMenu.delete")}</Text>
+            </Button>
+            <Button
+              variant="light"
+              childWrapperStyle={activityPopupMenuStyles.buttonChildContainer}
+              onPress={(): void => {
+                onPress({
+                  action: "cancel",
+                });
+              }}
+            >
+              <Icon
+                name="cancel"
+                style={activityPopupMenuStyles.buttonIcon}
+              />
+              <Text>{localization.get("activeProject.activityPopupMenu.cancel")}</Text>
+            </Button>
+          </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 }
