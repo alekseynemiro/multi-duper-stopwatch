90e340da28ec0a21fe8e6ec29cc4014d52a21bc9
Aleksey Nemiro <aleksey@kbyte.ru>
2023-08-28 17:12:25 +0300

    Create ReplaceModal

--- src/Views/Report/Components/index.ts
+++ src/Views/Report/Components/index.ts
@@ -1,5 +1,6 @@
 export * from "./CurrentActivity";
 export * from "./FilterModal";
+export * from "./ReplaceModal";
 export * from "./ReportViewItem";
 export * from "./ReportViewItemPopupMenu";
 export * from "./Total";
