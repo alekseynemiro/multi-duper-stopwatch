7fe06d46e4c239e8cc554baa3ec64daffb7792e7
Aleksey Nemiro <aleksey@kbyte.ru>
2023-08-29 14:21:43 +0300

    HorizontalLineStyles - Fix styles

--- src/Components/HorizontalLine/HorizontalLineStyles.ts
+++ src/Components/HorizontalLine/HorizontalLineStyles.ts
@@ -4,18 +4,17 @@ import { colors } from "@styles";
 export const horizontalLineStyles = StyleSheet.create({
   default: {
     borderBottomColor: colors.border,
-    borderBottomWidth: StyleSheet.hairlineWidth,
   },
   lg: {
     marginVertical: 16,
-    borderWidth: 1.5,
+    borderBottomWidth: 1.5,
   },
   md: {
-    marginVertical: 8,
-    borderWidth: 1,
+    marginVertical: 16,
+    borderBottomWidth: 1,
   },
   sm: {
-    marginVertical: 4,
-    borderWidth: 0.5,
+    marginVertical: 8,
+    borderBottomWidth: 0.5,
   },
 });
